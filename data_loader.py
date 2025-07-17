14f64b170c8ee60f1a1cb014366375980aa5c7e3
Fernando-rota <analistafrotarota@gmail.com>
2025-07-17 15:07:31 -0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-def load_data(uploaded_file):
-    if uploaded_file:
-        return pd.read_excel(uploaded_file)
-    return None
+def load_checklist_data(uploaded_file):
+    if uploaded_file is not None:
+        try:
+            df = pd.read_excel(uploaded_file)
+            return df
+        except Exception as e:
+            raise ValueError(f"Erro ao ler o arquivo: {e}")
+    return pd.DataFrame()
+
